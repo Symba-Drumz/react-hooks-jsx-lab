07cb7dbcd0021fe9a7e9160ae11ce09d8eb1fdb8
alfazachmmosi@TimeMachine <zalfa3890@gmail.com>
2025-04-14 04:14:08 +0300

    modifies files: About.js, Home.js, NavBar.js, data.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const headStyle = {
+    color: 'firebrick'
+  };
+
+  return (
+    <div id="home">
+      <h1 style={headStyle}>Alfa is a Web Developer from Nairobi</h1>
+    </div>
+  )
 }
 
 export default Home;
